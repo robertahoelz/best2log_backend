189a9547867087201a596bdad3457e9d8c27c8da
Roberta Rocha Hoelz <robertahoelz@hotmail.com>
2022-07-11 16:42:24 -0300

    criação da API

---
+++ src/main/java/com/best2log/best2log/Best2logApplication.java
@@ -0,0 +1,13 @@
+package com.best2log.best2log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Best2logApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Best2logApplication.class, args);
+	}
+
+}
